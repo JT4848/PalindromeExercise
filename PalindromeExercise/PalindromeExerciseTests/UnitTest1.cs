e0fddd9298c249bc120cc16b13428cbf2a869032
John Thomas Pricone <jtpricone@gmail.com>
2023-01-11 19:21:34 -0500

    done

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("deified", true)]
+        [InlineData("apple", false)]
+        public void Test1(string word, bool expected)
         {
-
+            var instance = new WordSmith();
+            var actual = instance.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
